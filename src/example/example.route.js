64d076141dd2cf8ef330ad0c3468a7f9736ee160
Douglas Alves <douglas.expalves@gmail.com>
2017-08-01 16:33:06 -0300

    rota get de exemplo com parametro

--- src/example/example.route.js
+++ src/example/example.route.js
@@ -2,4 +2,8 @@ module.exports = function rotaExample(application) {
   application.get('/api/example', (req, res) => {
     application.src.example.model.example(application, req, res);
   });
+
+  application.get('/api/example/:id', (req, res) => {
+    res.status(200).json(`Seu Id de exemplo é: ${req.params.id}`);
+  });
 };
