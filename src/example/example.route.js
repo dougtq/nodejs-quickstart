4d99b1cf8bf0fde139ef419530d6b89b58cf7ece
Douglas Alves <douglas.expalves@gmail.com>
2017-08-01 22:29:42 -0300

    alteração nas rotas example -- examples, exemplo de uso de apidoc

--- src/example/example.route.js
+++ src/example/example.route.js
@@ -1,25 +1,61 @@
 module.exports = function rotaExample(application) {
-  application.get('/api/example', (req, res) => {
-    application.src.example.controller.example(application, req, res);
+  /* API DOC DA ROTA */
+  application.get('/api/examples', (req, res) => {
+    // application.src.example.controller.example(application, req, res);
   });
 
-  application.get('/api/example/:id', (req, res) => {
+  /**
+   * @api {get} /api/examples/:id GetExample
+   * @apiName GetExample
+   * @apiGroup Example
+   * @apiPermission user
+   * @apiHeader {Token} Authorization Token JWT.
+   * @apiHeaderExample Header-Example:
+   * {
+   *   "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOjQsImV4cCI6MTQ5NDU5NjY2MDg0OH0.oW4RNq7AuHuUqPNcuCBy5LP_L-hbqCoH4qF8Q-xNf2w"
+   * }
+   * @apiExample {curl} Example usage:
+   *  curl -i http://localhost/api/examples/7
+   * @apiParam {Number} id Example_Id do example.
+   *
+   *
+   *
+   * @apiSuccess (200) {String} descricao - descricao do exemplo
+   * @apiSuccess (200) {Boolean} ativo Estado do exemplo se está ativa ou cancelada
+   * @apiSuccess (200) {Int} tipo Tipo do exemplo
+   *
+   * @apiSuccessExample {json} Sucesso
+   *    HTTP/1.1 200 OK
+   *    {
+   *      "descricao" : "Examplo Default"
+   *      "ativo" : "1"
+   *      "tipo" : "2"
+   *    }
+   * @apiError (404) {Error} SQLTIMEOUT Erro com SQL
+   *
+   * @apiErrorExample Error-Response:
+   *    HTTP/1.1 404 Not Found
+   *    {
+   *      "error": "Falha na API"
+   *    }
+   */
+  application.get('/api/examples/:id', (req, res) => {
     res.status(200).json(`Seu Id de exemplo é: ${req.params.id}`);
   });
 
-  application.post('/api/example/', (req, res) => {
+  application.post('/api/examples/', (req, res) => {
     res.status(200).json('Exemplo a ser criado ');
   });
 
-  application.put('/api/example/:id', (req, res) => {
+  application.put('/api/examples/:id', (req, res) => {
     res.status(200).json(`Seu Id de exemplo a ser alterado é: ${req.params.id}`);
   });
 
-  application.patch('/api/example/:id', (req, res) => {
+  application.patch('/api/examples/:id', (req, res) => {
     res.status(200).json(`Seu Id de exemplo a ser alterado parcialmente é: ${req.params.id}`);
   });
 
-  application.delete('/api/example/:id', (req, res) => {
+  application.delete('/api/examples/:id', (req, res) => {
     res.status(200).json(`Seu Id de exemplo a ser deletado é: ${req.params.id}`);
   });
 };
