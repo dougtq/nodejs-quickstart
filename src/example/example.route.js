ef4b70d7ee4615b449ec421c1b5acd0f5a8c9c7b
Douglas Alves <douglas.expalves@gmail.com>
2017-08-01 21:59:59 -0300

    exemplos de rotas de varios metodos

--- src/example/example.route.js
+++ src/example/example.route.js
@@ -6,4 +6,20 @@ module.exports = function rotaExample(application) {
   application.get('/api/example/:id', (req, res) => {
     res.status(200).json(`Seu Id de exemplo é: ${req.params.id}`);
   });
+
+  application.post('/api/example/', (req, res) => {
+    res.status(200).json('Exemplo a ser criado ');
+  });
+
+  application.put('/api/example/:id', (req, res) => {
+    res.status(200).json(`Seu Id de exemplo a ser alterado é: ${req.params.id}`);
+  });
+
+  application.patch('/api/example/:id', (req, res) => {
+    res.status(200).json(`Seu Id de exemplo a ser alterado parcialmente é: ${req.params.id}`);
+  });
+
+  application.delete('/api/example/:id', (req, res) => {
+    res.status(200).json(`Seu Id de exemplo a ser deletado é: ${req.params.id}`);
+  });
 };
