69e690da772ecc985e0fecd70ce24409c9b3a178
Douglas Alves <douglas.expalves@gmail.com>
2017-08-01 23:50:34 -0300

    mssql poolconnection, request  e query

---
+++ src/config/mssql.js
@@ -0,0 +1,42 @@
+const sql = require('mssql');
+
+let query;
+
+const Executioner = function mssqlExec(conf, req, res) {
+  try {
+    const pool = new sql.ConnectionPool(conf, (err) => {
+      pool.on('error', (poolErr) => {
+        global.winston('debug', `At: ${new Date()} error:${poolErr}`);
+        return res.status(500).json({ message: 'Erro com a conexão' });
+      });
+
+      if (err) {
+        global.winston('debug', `At: ${new Date()} error:${err}`);
+        return res.status(500).json({ message: 'Erro com a conexão' });
+      }
+
+      const request = new sql.Request(pool);
+      request.on('error', (reqErr) => {
+        global.winston('debug', `At: ${new Date()} error:${reqErr}`);
+        return res.status(500).json({ message: 'Erro com a conexão' });
+      });
+
+      request.query(req, (queryErr, data) => {
+        pool.close();
+        if (queryErr) {
+          global.winston('debug', `At: ${new Date()} error:${queryErr}`);
+          return res.status(500).json({ message: 'Erro com a conexão' });
+        }
+        query = data;
+      });
+    });
+  } catch (error) {
+    global.winston('debug', `At: ${new Date()} error:${error}`);
+    return res.status(500).json({ message: 'Erro com a conexão' });
+  }
+  return res(query);
+};
+
+module.exports = function exporta() {
+  return Executioner;
+};
