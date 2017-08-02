9d3613aa23d442f989d9cf6884a819f6781ac5e8
Douglas Alves <douglas.expalves@gmail.com>
2017-08-01 22:30:43 -0300

    reorganização de lógica de execução do arquivo

--- server.js
+++ server.js
@@ -2,13 +2,11 @@ import app from './src/config/express';
 
 try {
   app.listen(app.set('port'), () => {
-    global.winston.log('debug', `Server rodando na porta ${app.set('port')}`);
-    // Remove winston logs from the console
-    global.winston.remove(global.winston.transports.Console);
-    // Create log file with the NODE_ENV's value
-    global.winston.add(global.winston.transports.File, { filename: `../../logs/API_${process.env.NODE_ENV}.log` });
     global.app = app;
+    global.winston.log('debug', `Server rodando na porta ${app.set('port')}`);
+    global.winston.add(global.winston.transports.File, { filename: `../../logs/API_${process.env.NODE_ENV}.log` });
+    global.winston.remove(global.winston.transports.Console);
   });
-} catch (e) {
-  global.winston.error('debug', `Erro ao iniciar server ${e}`);
+} catch (err) {
+  global.winston.error('debug', `Erro ao iniciar server: ${err}`);
 }
