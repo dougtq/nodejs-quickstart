03328d541f2b6829bebae3a767d958aab07e4f1f
Douglas Alves <douglas.expalves@gmail.com>
2017-08-02 09:19:36 -0300

    importação de env_mssql

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import app from './src/config/express';
+import env from './src/env/env_mssql';
 
 try {
   app.listen(app.set('port'), () => {
     global.app = app;
     global.winston.log('debug', `Server rodando na porta ${app.set('port')}`);
+    // console.log(global.env);
     global.winston.add(global.winston.transports.File, { filename: `../../logs/API_${process.env.NODE_ENV}.log` });
     global.winston.remove(global.winston.transports.Console);
   });
