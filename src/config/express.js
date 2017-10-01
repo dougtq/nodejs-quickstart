5980e30b0e07005cd20f876f3333d1234318b865
Douglas Alves <douglas.expalves@gmail.com>
2017-10-01 03:05:27 -0300

    mudanças estruturais

--- src/config/express.js
+++ src/config/express.js
@@ -1,39 +1,39 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import consign from 'consign';
-import winston from 'winston';
-import helmet from 'helmet';
-import method from 'method-override';
-import status from 'http-status';
+let express = require('express')
+let bodyParser = require('body-parser')
+let consign = require('consign')
+let winston = require('winston')
+let method = require('method-override')
+let status = require('http-status')
 
-const app = express();
-global.http = status;
-global.winston = winston;
-global.winston.level = 'debug';
-global.winston.add(global.winston.transports.File, { filename: `../../logs/API_${process.env.NODE_ENV}.log` });
+const app = express()
+global.http = status
+global.winston = winston
+global.winston.level = 'debug'
+global.winston.add(global.winston.transports.File, {
+  filename: `../../logs/API_${process.env.NODE_ENV}.log`
+})
 
 //  middleware
-app.set('port', 3000); // porta de acesso
+app.set('port', 3000) // porta de acesso
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
-  res.setHeader('Access-Control-Expose-Headers', 'Authorization');
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH')
   res.setHeader(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  )
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  next()
+})
 
-app.use(method());
-app.use(express.static('./dist'));
-app.use(bodyParser.urlencoded({ extended: true, limit: '5mb' }));
-app.use(bodyParser.json({ limit: '5mb' }));
-app.use(helmet());
+app.use(method())
+app.use(express.static('./dist'))
+app.use(bodyParser.urlencoded({ extended : true }))
+app.use(bodyParser.json())
 
 // carregamento de rotas, controllers e models
 consign({ verbose: true }) /* setting the verbose property as false */
-  .include('src/example/')
-  .into(app);
+  .include('src/authExample/auth.route.js')
+  .into(app)
 
-module.exports = app;
+module.exports = app
