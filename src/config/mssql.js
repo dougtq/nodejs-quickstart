eafbc65a73a337f299656c0cf7a0a71fcfd8e2a8
Douglas Alves <douglas.expalves@gmail.com>
2017-10-01 03:06:26 -0300

    sequelize para mssql

--- src/config/mssql.js
+++ src/config/mssql.js
@@ -1,42 +1,4 @@
-import mssql from 'mssql';
+let Sequelize = require('sequelize')
 
-let query;
 
-const Executioner = function mssqlExec(req, res) {
-  try {
-    const pool = new mssql.ConnectionPool(global.env, (err) => {
-      pool.on('error', (poolErr) => {
-        global.winston('debug', `At: ${new Date()} error:${poolErr}`);
-        return res.status(500).json({ message: 'Erro com a conexão' });
-      });
-
-      if (err) {
-        global.winston('debug', `At: ${new Date()} error:${err}`);
-        return res.status(500).json({ message: 'Erro com a conexão' });
-      }
-
-      const request = new mssql.Request(pool);
-      request.on('error', (reqErr) => {
-        global.winston('debug', `At: ${new Date()} error:${reqErr}`);
-        return res.status(500).json({ message: 'Erro com a conexão' });
-      });
-
-      request.query(req, (queryErr, data) => {
-        pool.close();
-        if (queryErr) {
-          global.winston('debug', `At: ${new Date()} error:${queryErr}`);
-          return res.status(404).json({ message: 'Erro com a execução' });
-        }
-        query = data;
-      });
-    });
-  } catch (error) {
-    global.winston('debug', `At: ${new Date()} error:${error}`);
-    return res.status(500).json({ message: 'Erro com a conexão' });
-  }
-  return res(query);
-};
-
-module.exports = function exporta() {
-  return Executioner;
-};
+module.exports =  new Sequelize('') //connect uri
